62500702f5282e1fea90c07411d07eec09c4d2f3
Amaury Chabert <chabert.amaury2002@gmail.com>
2024-03-21 13:05:52 +0100

    add test for user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\userController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route pour ajouter un utilisateur
+Route::post('/users', [userController::class, 'store']);
+
+// Route pour supprimer un utilisateur
+Route::delete('/users/{id}', [userController::class, 'destroy']);
